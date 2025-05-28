a179aaf8d7f7c3dbec1dd80c14934269d52cabaa
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-05-28 11:11:24 +0000

    configure wezterm

---
+++ configuration/home/wezterm/dot-config/wezterm/themes/matugen.lua
@@ -0,0 +1,29 @@
+return {
+    foreground = "#e1e2e9",
+    background = "#111318",
+    cursor_bg = "#e1e2e9",
+    cursor_border = "#e1e2e9",
+    cursor_fg = "#111318",
+    selection_bg = "#bcc7dc",
+    selection_fg = "#263141",
+    ansi = {
+        "#111318",
+        "#fbb0d8",
+        "#91d5ad",
+        "#d5c871",
+        "#a6c8ff",
+        "#afc6ff",
+        "#89d0ed",
+        "#e1e2e9"
+    },
+    brights = {
+        "#111318",
+        "#f97fc0",
+        "#6cc791",
+        "#c9b94a",
+        "#73a8ff",
+        "#7ca2ff",
+        "#5dbee6",
+        "#c3c6cf"
+    },
+}
